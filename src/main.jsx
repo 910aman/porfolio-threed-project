d5aafb9b71ba9334bd8743eb44874d134e3c504b
910aman <138217131+910aman@users.noreply.github.com>
2024-12-16 11:50:13 +0530

    portfolio completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Analytics } from '@vercel/analytics/react';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+    <Analytics />
+  </React.StrictMode>
+);
