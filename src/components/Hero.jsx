da3803e482779f936b9e799b85140a26b88b9f7d
910aman <138217131+910aman@users.noreply.github.com>
2024-12-16 21:24:46 +0530

    minor changes in hero page second time

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
   }, []);
 
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section className={`relative w-full h-[100lvh] mx-auto`}>
       <div className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
@@ -32,12 +32,10 @@ const Hero = () => {
       </div>
 
       <div className="hidden md:flex flex-col items-center justify-center w-full h-full">
-        <div className="w-full h-[500px] md:h-full">
-          <ComputersCanvas />
-        </div>
+        <ComputersCanvas />
       </div>
 
-      <div className='absolute sm:bottom-14 bottom-16 lg:bottom-20 w-full flex justify-center items-center'>
+      <div className='absolute sm:bottom-28 bottom-20 lg:bottom-20 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
