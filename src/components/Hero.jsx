bd2b80dd212ce41874b24b0d33c35cfbf05399e5
910aman <138217131+910aman@users.noreply.github.com>
2024-12-21 16:56:27 +0530

    added bug in computer canvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,9 +31,9 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="items-center justify-center w-full h-full">
+      {/* <div className="items-center justify-center w-full h-full"> */}
         <ComputersCanvas />
-      </div>
+      {/* </div> */}
 
       <div className='absolute sm:bottom-28 bottom-20 lg:bottom-20 w-full flex justify-center items-center'>
         <a href='#about'>
