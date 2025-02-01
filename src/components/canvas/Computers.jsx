4d10b940266058b207039bd14d0d2bebeb2fedd8
910aman <138217131+910aman@users.noreply.github.com>
2025-02-01 18:46:08 +0530

    new desktop computer pc add

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  const computer = useGLTF("./desktop_pc/threeDScene1.glb");
+  const computer = useGLTF("./new_desktop_pc/scene.gltf");
 
   return (
     <mesh>
