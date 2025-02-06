b053eeab89ed945241c4e52ecf71e666c603bb3a
910aman <138217131+910aman@users.noreply.github.com>
2025-02-06 13:10:35 +0530

    testing one the sitemap with google

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
