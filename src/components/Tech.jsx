42bfb2da29ce9a0d286bf1c8073aad60c3c670f4
910aman <138217131+910aman@users.noreply.github.com>
2025-03-29 23:55:37 +0530

    now revert again added the first 4 techs

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,11 +7,11 @@ import { technologies_1,technologies_2 } from "../constants";
 const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-start md:justify-center gap-x-20 gap-y-8'>
-      {/* {technologies_1.map((technology) => (
+      {technologies_1.map((technology) => (
         <div className='md:w-40 md:h-40 w-28 h-28' key={technology.name} title={technologies_1.title}>
           <BallCanvas icon={technology.icon} />
         </div>
-      ))} */}
+      ))}
       {technologies_2.map((technology) => (
         <div className='md:w-40 md:h-40 w-28 h-28' key={technology.name} title={technologies_2.title}>
           <BallCanvas icon={technology.icon} />
