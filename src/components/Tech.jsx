f3c9fb424c6b64ee6172f223d001dfe9a38a9eef
910aman <138217131+910aman@users.noreply.github.com>
2025-03-30 00:02:38 +0530

    trying to solve problem 2

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,7 +6,7 @@ import { technologies_1,technologies_2 } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-start md:justify-center gap-40 gap-x-20 gap-y-8'>
+    <div className='flex flex-row flex-wrap justify-start md:justify-center gap-x-40 gap-y-8'>
       {technologies_1.map((technology) => (
         <div className='md:w-40 md:h-40 w-28 h-28' key={technology.name} title={technology.title}>
           <BallCanvas icon={technology.icon} />
